fda209baded348e9aa2a905d077b52822f2b515d
Tina Lambert <lambert.tinamarie@gmail.com>
2024-10-14 20:44:15 -0700

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,34 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+   const loggedIn = { firstName: 'Tina', lastName: 'Is Awesome', email: 'TinaIsAwesome@awesome.com' }
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+         <header className='home-header'>
+            <HeaderBox 
+               type="greeting"
+               title="Welcome"
+               user={loggedIn?.firstName || 'Guest'}
+               subtext="Access and manage your account and transactions efficiently."
+            />
+            <TotalBalanceBox 
+               accounts={[]}
+               totalBanks={1}
+               totalCurrentBalance={1250.35}
+            />
+         </header>
+         RECENT TRANSACTIONS
+      </div>
+      <RightSidebar 
+         user={loggedIn}
+         transactions={[]}
+         banks={[{currentBalance: 888.80}, {currentBalance: 333.30}]}
+      />
+    </section>
   )
 }
 
